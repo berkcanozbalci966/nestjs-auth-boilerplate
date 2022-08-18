c8f02c8f8c301bb32b023f345ca14bd81dd67755
unknown <berkcanozbalci@gmail.com>
2022-08-18 16:31:36 +0300

    validation pipe added

--- server/src/auth/dto/sign-user.dto.ts
+++ server/src/auth/dto/sign-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString, Length, Min } from 'class-validator';
+
 export class SignInUserDto {
+  @IsEmail()
   email?: string;
+
+  @IsString()
+  @Length(8, 30)
   username?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(8, 30)
   password: string;
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3600).then(() => {
-    console.log('Fuck yeah');
-  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3600);
 }
 bootstrap();
